3e65c024350c13f8cf02ee463f6f3768098a21eb
brianwhardwick <brianwhardwick@gmail.com>
2026-01-07 14:20:38 +1100

    Create header.php

---
+++ assets/components/header.php
@@ -0,0 +1,24 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJ12ZW06K4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-LJ12ZW06K4');
+</script>
+
+<nav class="site-nav">
+    <div class="nav-content">
+        <a href="https://www.projectgvm.com.au/home" class="nav-brand">
+            <img src="logo_white.svg" alt="Project GVM">
+            <span>Project GVM</span>
+        </a>
+
+        <div class="nav-links">
+            <a href="https://www.projectgvm.com.au/home">Home</a>
+            <a href="https://www.projectgvm.com.au/calculator">Calculator</a>
+            <a href="https://www.projectgvm.com.au/learn/understanding-gvm">Learn</a>
+            <a href="https://www.projectgvm.com.au/FAQ">FAQs</a>
+            <a href="https://www.projectgvm.com.au/about">About</a>
+        </div>
+    </div>
+</nav>
