462b4b8d3c23b0582503ff4900128cd42e296983
brianwhardwick <brianwhardwick@gmail.com>
2026-01-07 14:21:08 +1100

    Create footer.php

---
+++ assets/components/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="footer-links">
+    <a href="https://www.projectgvm.com.au/about">ABOUT</a>
+    <span class="footer-separator">|</span>
+    
+    <a href="https://www.projectgvm.com.au/about#contact">CONTACT US</a>
+    <span class="footer-separator">|</span>
+
+    <a href="https://www.projectgvm.com.au/disclaimer">DISCLAIMER</a>
+    <span class="footer-separator">|</span>
+
+    <a href="https://www.projectgvm.com.au/privacy-policy">PRIVACY POLICY</a>
+    <span class="footer-separator">|</span>
+       
+    <a href="https://www.projectgvm.com.au/Terms-and-Conditions">TERMS AND CONDITIONS</a>
+  </div>
+
+  <div class="footer-copyright">
+    &copy; 2025-<span id="year"></span> Project GVM. All rights reserved.
+  </div>
+</footer>
+
+<script src="/script.js"></script>
+
+<script>
+  document.getElementById("year").textContent = new Date().getFullYear();
+</script>
