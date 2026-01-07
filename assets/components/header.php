39248410343ac66c16c2c692c9e9600f174dd419
brianwhardwick <brianwhardwick@gmail.com>
2026-01-07 15:04:48 +1100

    Update header.php

--- assets/components/header.php
+++ assets/components/header.php
@@ -1,24 +1,13 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJ12ZW06K4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-LJ12ZW06K4');
-</script>
-
-<nav class="site-nav">
-    <div class="nav-content">
-        <a href="https://www.projectgvm.com.au/home" class="nav-brand">
-            <img src="logo_white.svg" alt="Project GVM">
+<nav class="navbar">
+    <div class="navbar-content">
+        <a href="/" class="brand">
+            <img src="/assets/img/brand/logo_white.svg" alt="Project GVM">
             <span>Project GVM</span>
         </a>
-
-        <div class="nav-links">
-            <a href="https://www.projectgvm.com.au/home">Home</a>
-            <a href="https://www.projectgvm.com.au/calculator">Calculator</a>
-            <a href="https://www.projectgvm.com.au/learn/understanding-gvm">Learn</a>
-            <a href="https://www.projectgvm.com.au/FAQ">FAQs</a>
-            <a href="https://www.projectgvm.com.au/about">About</a>
-        </div>
+        <ul class="nav-menu">
+            <li><a href="/index.html" class="nav-link">Home</a></li>
+            <li><a href="/about.html" class="nav-link">About</a></li>
+            <li><a href="/faq.html" class="nav-link">FAQs</a></li>
+        </ul>
     </div>
 </nav>
